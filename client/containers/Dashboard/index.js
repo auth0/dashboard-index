e7b1d8f900799005a6ca68ca9fb9417d065a3fe3
Cristian Douce <cristian@gravityonmars.com>
2016-04-13 10:30:35 -0300

    Fixes fetch

--- client/containers/Dashboard/index.js
+++ client/containers/Dashboard/index.js
@@ -8,21 +8,14 @@ import MainSection from '../../components/MainSection'
 import {AUTH0_API_TOKEN, AUTH0_DOMAIN, AUTH0_API_URL} from '../../constants/env'
 
 class Dashboard extends Component {
-  constructor() {
-    super()
-    this.stats = {}
+  constructor(props, context) {
+    super(props, context)
+    this.state = {stats: {}, users: {}}
   }
-  componentDidMount() {
-    const api_url = AUTH0_API_URL.replace('{tenant_domain}', AUTH0_DOMAIN)
 
-    fetch(`${api_url}/stats/daily`, {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${AUTH0_API_TOKEN}`
-      }
-    })
-    .then((response) => response.json())
-    .then((stats) => console.log(stats))
+  componentDidMount() {
+    this.fetchUsers()
+    this.fetchStats()
   }
 
   render() {
@@ -146,13 +139,42 @@ class Dashboard extends Component {
             <Sidebar currentRoute={"/"} />
             <div className="col-xs-10 wrapper">
               <div id="content">
-                <MainSection />
+                <MainSection
+                  stats={this.state.stats}
+                  users={this.state.users}
+                />
               </div>
             </div>
           </div>
         </div>
       </span>
     )
+  }
+
+  fetchUsers() {
+    const api_url = AUTH0_API_URL.replace('{tenant_domain}', AUTH0_DOMAIN)
+
+    fetch(`${api_url}/stats/daily`, {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${AUTH0_API_TOKEN}`
+      }
+    })
+    .then((response) => response.json())
+    .then((users) => this.setState({ users }))
+  }
+
+  fetchStats() {
+    const api_url = AUTH0_API_URL.replace('{tenant_domain}', AUTH0_DOMAIN)
+
+    fetch(`${api_url}/users`, {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${AUTH0_API_TOKEN}`
+      }
+    })
+    .then((response) => response.json())
+    .then((stats) => this.setState({ stats }))
   }
 }
 
